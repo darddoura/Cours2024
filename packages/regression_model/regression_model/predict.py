6b688ec75f57699cc7a5a7c7d71aa167dff725d4
darddoura <mediouni.dorra@gmail.com>
2024-12-10 16:19:51 -0500

    Partie-3-Ajout prédictions et test

--- packages/regression_model/regression_model/predict.py
+++ packages/regression_model/regression_model/predict.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 from regression_model.processing.data_management import load_pipeline
 from regression_model.config import config
-from regression_model.processing.validation import validate_inputs
 
 
 pipeline_file_name = "regression_model.pkl"
@@ -14,8 +13,7 @@ def make_prediction(*, input_data) -> dict:
     """Make a prediction using the saved model pipeline."""
 
     data = pd.read_json(input_data)
-    validated_data = validate_inputs(input_data=data)
-    prediction = _price_pipe.predict(validated_data[config.FEATURES])
+    prediction = _price_pipe.predict(data[config.FEATURES])
     output = np.exp(prediction)
     response = {"predictions": output}
 
