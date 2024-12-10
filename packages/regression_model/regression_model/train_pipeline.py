1a6b75895bc293900842ddba3a3a13151253a766
darddoura <mediouni.dorra@gmail.com>
2024-12-10 16:22:51 -0500

    Partie-4-Ajout validation de données

--- packages/regression_model/regression_model/train_pipeline.py
+++ packages/regression_model/regression_model/train_pipeline.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Ajoutez dynamiquement le chemin du parent au PYTHONPATH
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import numpy as np
 from sklearn.model_selection import train_test_split
 
