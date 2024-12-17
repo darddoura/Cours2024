c5c907633d2e6045dfb22febb15bd17cf1efe873
darddoura <mediouni.dorra@gmail.com>
2024-12-17 15:25:54 -0500

    partie-12-Ajout-Schema de validation-API flask

--- packages/regression_model/regression_model/predict.py
+++ packages/regression_model/regression_model/predict.py
@@ -18,7 +18,12 @@ _price_pipe = load_pipeline(file_name=pipeline_file_name)
 def make_prediction(*, input_data) -> dict:
     """Make a prediction using the saved model pipeline."""
 
-    data = pd.read_json(input_data)
+    # Vérifie si l'entrée est une chaîne JSON ou une liste/dictionnaire
+    if isinstance(input_data, (list, dict)):
+        data = pd.DataFrame(input_data)  # Convertir directement en DataFrame
+    else:
+        data = pd.read_json(input_data)  # Lire si c'est une chaîne JSON
+    #data = pd.read_json(input_data)
     validated_data = validate_inputs(input_data=data)
     prediction = _price_pipe.predict(validated_data[config.FEATURES])
     output = np.exp(prediction)
